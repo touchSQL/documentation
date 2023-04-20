a6818337e6833ec6f8d62b07d14a458725f2d845
touchSQL <130754200+touchSQL@users.noreply.github.com>
2023-04-21 02:16:15 +0530

    Update python-integration-post-data.py

--- python-integration-post-data.py
+++ python-integration-post-data.py
@@ -6,7 +6,7 @@ import re
 
 query_c = "insert into `test` (`name`,`lname`) values ('Gopal','Dubey')"
 query = re.sub(r"\s", "+", query_c)
-my_request = urllib.request.urlopen("https://apiin.touchsql.com/?database=myDB&api=Your_API&key=Your_Key&query="+query)
+my_request = urllib.request.urlopen("https://post.touchsql.com/?database=myDB&api=Your_API&key=Your_Key&query="+query)
 
 my_HTML = my_request.read().decode("utf8")
 # a dictionary
