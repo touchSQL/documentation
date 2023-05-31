529ce06d64672bc9a19e49e272ef3f6df881d2f8
touchSQL <130754200+touchSQL@users.noreply.github.com>
2023-05-31 12:10:01 +0530

    Update php-integration-post-data.php

--- php-integration-post-data.php
+++ php-integration-post-data.php
@@ -5,8 +5,11 @@ $name = $_POST["name"];
 $lname = $_POST["lname"];
 $query_c = "INSERT INTO `test`(`name`,`lname`) VALUES ('$name','$lname')";
 $query = preg_replace("/ /i", "+", $query_c);
-$url = "https://post.touchsql.com/?database=mydb&api=Your_API&key=Your_Key&query=$query";
-$ch = curl_init();
+
+// If you use query encryption, you will need to use the following URL: "https://post.touchsql.com/v2/?database=mydb&api=Your_API&key=Your_Key&query=$query".
+
+$url = "https://post.touchsql.com/?database=mydb&api=Your_API&key=Your_Key&query=$query";  // Use can also use file_get_contents(); method
+$ch = curl_init();   // Use can also use file_get_contents(); method
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_URL, $url);
 $exec = curl_exec($ch);
@@ -17,10 +20,10 @@ echo "</pre>";
 
 if($data["TRUE"] == "TRUE")
 {
-echo "Data inserted";
+    echo "Data inserted";
 }
 elseif($data["FALSE"] == "FALSE"){
-echo "Try again";
+    echo "Try again";
 }
 }
 ?>
